2d27d3c7e450324a81fb45b3ca1efd91f6b803ad
Lyxell <42148169+adrlyx@users.noreply.github.com>
2022-10-17 22:52:33 +0200

    Update variables.tf

--- tf/modules/cloudrun/variables.tf
+++ tf/modules/cloudrun/variables.tf
@@ -1,9 +1,3 @@
-# variable "annotations" {
-#   type = map(string)
-# }
-# variable "annotations_spec" {
-#   type = map(string)
-# }
 variable "app_name" {
   type = string
 }
@@ -11,27 +5,6 @@ variable "app_name" {
 variable "environment" {
   type = string
 }
-
-# variable "args" {
-#   type = list(string)
-# }
-
-# variable "department_name" {
-#   type = string
-# }
-
-# variable "enable_vpc_connector" {
-#   type    = bool
-#   default = false
-# }
-
-# variable "environment" {
-#   type = string
-# }
-
-# variable "google_project" {
-#   type = any
-# }
 
 variable "location" {
   type = string
@@ -44,32 +17,3 @@ variable "project_id" {
 variable "cloud_run_sa" {
   type = string
 }
-
-
-# variable "global" {
-#   type = map(any)
-# }
-
-# variable "paper" {
-#   type = map(any)
-# }
-
-# variable "svc_dep" {
-#   type = any
-# }
-
-# variable "vpc_dep" {
-#   type = any
-# }
-
-# variable "redis_connection_string" {
-#   type = any
-# }
-
-# variable "cpu" {
-#   type = string
-# }
-
-# variable "memory" {
-#   type = string
-# }
